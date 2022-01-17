53a44c4c71de3c55a321883e6b9868a36f9e2d64
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2022-01-17 17:42:31 +0100

    testing useeffect with objects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [person , setPerson] = useState({
+    firstName: 'Rizwan 00',
+    lastName: 'Mushtaq 00'
+  })
+  
+  const clickHandlerFirstName = () => {
+    setPerson( (person) => ({
+      ...person,
+      firstName: 'Rizwan ' + Math.floor( Math.random() * (100 - 1) + 1)
+    }))
+  }
+
+  const clickHandlerLastName = () => {
+    setPerson( (person) => ({
+      ...person,
+      lastName: 'Mushtaq ' + Math.floor( Math.random() * (100 - 1) + 1)
+    }))
+  }
+
+  useEffect(() => {
+    console.log('running effect')
+  }, [person.firstName])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test App</h1>
+      <div>{JSON.stringify(person)}</div>
+      <button 
+        onClick={clickHandlerFirstName}
+      >Click</button>
+      <div>{JSON.stringify(person)}</div>
+      <button 
+        onClick={clickHandlerLastName}
+      >Click</button>
     </div>
   );
 }
